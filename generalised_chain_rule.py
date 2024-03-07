f902de5118cd509a48a57ed3c1687c5b63f6b185
John Reynolds <jjmreynolds@gmail.com>
2024-03-07 18:43:58 +0100

    feat: compute find dependent variable differentials

--- generalised_chain_rule.py
+++ generalised_chain_rule.py
@@ -139,8 +139,8 @@ def generalised_chain_rule(w: sp.Function, variables: dict[sp.Symbol, sp.Expr], 
     input for another function. The generalized chain rule extends
     the basic chain rule to functions with multiple variables
     and composite arguments.
-    
-    The simple chain rule extended to functions of more than one independent 
+
+    The simple chain rule extended to functions of more than one independent
     variable, in which each independent variable may depend on one or more other variables
 
     Let w=f(x1,x2,…,xm) be a differentiable function of m  independent
@@ -232,6 +232,42 @@ def chain_rule_derivative(expr: sp.Expr, variables: dict[sp.Symbol, sp.Expr], *a
     ]
 
     return result
+
+
+def find_dependent_variable_differentials(
+    w: sp.Function, variables: dict[sp.Symbol, sp.Expr], *args
+):
+    """Find the intermediate differentials of the function
+
+    Args:
+        w (sp.Function): function symbol to be differentiated
+        variables (dict[sp.Symbol, sp.Expr]): dictionary of variables
+            with their function definitions
+        args: independent variables
+
+    Returns:
+        list[sp.Eq]: list of equations giving function  differentiated
+            wrt args
+
+    Examples:
+        >>> x, y, r, theta = sp.symbols("x, y, r, theta", real=True)
+        >>> dependents = {x: r * sp.cos(theta), y: r * sp.sin(theta)}
+        >>> f = sp.Function("f")
+        >>> result = find_dependent_variable_differentials(f, dependents, r, theta)
+
+        >>> result[sp.Derivative(f(x, y), x)]
+        cos(theta)*Derivative(f(r, theta), r) - sin(theta)*Derivative(f(r, theta), theta)/r
+
+        >>> result[sp.Derivative(f(x, y), y)]
+        sin(theta)*Derivative(f(r, theta), r) + cos(theta)*Derivative(f(r, theta), theta)/r
+
+    """
+    independent_variable_differentials = generalised_chain_rule(w, variables, *args)
+    differentials = [(w(*variables.keys()).diff(xi)) for xi in variables.keys()]
+    return {
+        a: b.simplify()
+        for a, b in sp.solve(independent_variable_differentials, differentials).items()
+    }
 
 
 if __name__ == "__main__":
