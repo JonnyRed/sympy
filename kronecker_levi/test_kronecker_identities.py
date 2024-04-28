d2d351836698dad354d9e204bcf500993ce46d3a
John Reynolds <jjmreynolds@gmail.com>
2024-04-28 20:09:11 +0200

    fix: Bug in indices of function
    fix; index should be n+1
    docs: improve documentation of test functions

--- kronecker_levi/test_kronecker_identities.py
+++ kronecker_levi/test_kronecker_identities.py
@@ -1,38 +1,41 @@
 import kronecker_levi.kronecker_delta as kd
 
+
 def kronecker_product(n: int):
     """
-    Generate pairs of Kronecker delta products and their summation.
+     Generate pairs of Kronecker delta products and their summation.
 
-    The function yields tuples containing the Kronecker delta of 
-    `i` and `k`, and the summation of the product of Kronecker deltas 
-    over `j`. The fuction yiels a tuple consisting of 
-    delta_ij*delta_jk (using einstein summation convention) and
-    delta_ik. For each of these:
-    
-    delta_ij*delta_jk = delta_ik 
+     The function yields tuples containing the Kronecker delta of
+     `i` and `k`, and the summation of the product of Kronecker deltas
+     over `j`. The fuction yiels a tuple consisting of
+     delta_ij*delta_jk (using einstein summation convention) and
+     delta_ik. For each of these:
 
-    Parameters:
-        n (int): The upper limit for the range of indices.
+     delta_ij*delta_jk = delta_ik
 
-    Yields:
-        tuple: A tuple containing the Kronecker delta of `i` and `k`, 
-        and the summation of delta_ij*delta_jk.
+     Parameters:
+         n (int): The upper limit for the range of indices.
 
-   Examples:
-    >>> list(kronecker_product(2))
-    [(1, 1), (0, 0), (0, 0), (1, 1)]
-    
+     Yields:
+         tuple: A tuple containing the Kronecker delta of `i` and `k`,
+         and the summation of delta_ij*delta_jk.
+
+    Examples:
+     >>> list(kronecker_product(2))
+     [(1, 1), (0, 0), (0, 0), (1, 1)]
+
     """
-    for i in range(1, n+1):
-        for k in range(1, n+1):
+    for i in range(1, n + 1):
+        for k in range(1, n + 1):
             delta_ik = kd.kronecker_delta(i, k)
             sigma = sum(
-                kd.kronecker_delta(i, j) * kd.kronecker_delta(j, k) 
-                    for j in range(1, n+1))
+                kd.kronecker_delta(i, j) * kd.kronecker_delta(j, k)
+                for j in range(1, n + 1)
+            )
             yield delta_ik, sigma
 
-def test_kronecker_product(n: int)-> bool:
+
+def test_kronecker_product(n: int) -> bool:
     """
         Test the kronecker_product function is satisfied.
 
@@ -41,17 +44,20 @@ def test_kronecker_product(n: int)-> bool:
 
     Returns:
     True if the kronecker identity is satisfied false otherwise
-    
+
     Examples:
     >>> test_kronecker_product(2)
     True
-    
+
     >>> test_kronecker_product(3)
     True
     """
     # generator = kronecker_product(n)
     return all(delta_ik == sigma for delta_ik, sigma in kronecker_product(n))
 
+
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod(verbose=True)
+    
