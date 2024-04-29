8f5f12a45725ea9f640ba39ff3c9442271c984ca
John Reynolds <jjmreynolds@gmail.com>
2024-04-29 02:03:52 +0200

    feat: implement generate tuples
    feat: Generate tuples for use in kronecker delta

--- kronecker_levi/kronecker_delta.py
+++ kronecker_levi/kronecker_delta.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def kronecker_delta(i: int, j: int) -> int:
     """
     The Kronecker delta function returns 1 if i equals j, and 0 otherwise
@@ -25,6 +28,28 @@ def kronecker_delta(i: int, j: int) -> int:
     0
     """
     return 1 if i == j else 0
+
+
+def generate_tuples(n):
+    """
+    Generate tuples of length n containing integers from 1 to n.
+
+    Args:
+        n (int): The length of each tuple.
+
+    Returns:
+        tuple: A tuple of tuples, where each inner tuple contains integers
+        from 1 to n.
+
+    Examples:
+        >>> generate_tuples(2)
+        ((1, 1), (1, 2), (2, 1), (2, 2))
+
+        >>> generate_tuples(3) #doctest: +ELLIPSIS
+        ((1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), ..., (3, 3, 1), (3, 3, 2), (3, 3, 3))
+
+    """
+    return tuple(itertools.product(range(1, n + 1), repeat=n))
 
 
 if __name__ == "__main__":
