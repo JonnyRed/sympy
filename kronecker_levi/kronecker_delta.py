67afc492566d2fa3df32d6a40a7c05c0a18edbc7
John Reynolds <jjmreynolds@gmail.com>
2024-05-09 20:01:42 +0200

    feat: add kronecker delta matrix

--- kronecker_levi/kronecker_delta.py
+++ kronecker_levi/kronecker_delta.py
@@ -45,6 +45,7 @@ This module provides two functions: `kronecker_delta` and `generate_tuples`.
 """
 
 import itertools
+from sympy import Matrix, KroneckerDelta
 
 
 def kronecker_delta(i: int, j: int) -> int:
@@ -92,10 +93,37 @@ def generate_tuples(n):
         ((1, 1), (1, 2), (2, 1), (2, 2))
 
         >>> generate_tuples(3) #doctest: +ELLIPSIS
-        ((1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), ..., (3, 3, 1), (3, 3, 2), (3, 3, 3))
+       ((1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), ..., (3, 3, 1), (3, 3, 2), (3, 3, 3))
 
     """
     return tuple(itertools.product(range(1, n + 1), repeat=n))
+
+def kronecker_delta_matrix(i, j, k, ell, m, n):
+    """
+    Calculates the determinant of a 3x3 matrix filled with Kronecker delta functions.
+
+    Args:
+        i (int): The first index for the Kronecker delta functions.
+        j (int): The second index for the Kronecker delta functions.
+        k (int): The third index for the Kronecker delta functions.
+        ell (int): The fourth index for the Kronecker delta functions.
+        m (int): The fifth index for the Kronecker delta functions.
+        n (int): The sixth index for the Kronecker delta functions.
+
+    Examples:
+
+    Returns:
+        int: The determinant of the matrix (always 6 for any non-zero arguments).
+    """
+
+    matrix = Matrix(
+        [
+            [KroneckerDelta(i, ell), KroneckerDelta(i, m), KroneckerDelta(i, n)],
+            [KroneckerDelta(j, ell), KroneckerDelta(j, m), KroneckerDelta(j, n)],
+            [KroneckerDelta(k, ell), KroneckerDelta(k, m), KroneckerDelta(k, n)],
+        ]
+    )
+    return matrix.det()
 
 
 if __name__ == "__main__":
